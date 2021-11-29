04c7d3ef1e86843ad074e8b691ca732260f3a3ea
Vlasov Artem [Lenovo] <root@proscript.ru>
2021-11-29 10:16:54 +0400

    uuid type

---
+++ property_mapper/types/uuid.py
@@ -0,0 +1,25 @@
+from typing import List
+from uuid import UUID as OrigUUID
+
+from .mapper_type import PropertyMapperType
+
+__all__ = ['UUID']
+
+
+class UUIDType(type):
+
+    def __new__(cls, *args, **kwargs):
+        class _UUID(PropertyMapperType):
+            allow_type: List = (str,)
+
+            def __call__(self, value):
+                if value is not None:
+                    return OrigUUID(value)
+                else:
+                    return None
+
+        return _UUID
+
+
+class UUID(OrigUUID, metaclass=UUIDType):
+    pass
