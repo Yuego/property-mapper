4c890c76d558a300185cf69fa3144aa91bd89912
Vlasov Artem [WS] <root@proscript.ru>
2025-11-21 18:20:46 +0400

    bump version 1.0.10

--- property_mapper/__init__.py
+++ property_mapper/__init__.py
@@ -2,6 +2,6 @@ from .interface import MapperInterface
 from .magic import MagicMapper
 from .mapper import PropertyMapper
 
-from .version import version
+from .version import version, build
 
-__version__ = version
+__version__ = f'{version}.{build}'
